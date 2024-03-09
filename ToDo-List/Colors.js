e2e1a718d3f02b1407fbde80606327a2a39f666b
falakn2405 <falakn2405@gmail.com>
2024-03-09 12:35:08 +0500

    Updated

--- ToDo-List/Colors.js
+++
@@ -1,9 +0,0 @@
-export default colors = {
-    black: '#2D3436',
-    gray: '#524e4e',
-    lightGray: '#CACACA',
-    blue: '#24A6D9',
-    lightBlue: '#A7CBD9',
-    red: '#D85963',
-    white: '#FFFFFF',
-}
